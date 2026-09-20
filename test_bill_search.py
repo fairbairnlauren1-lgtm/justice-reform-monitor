918c2d8cee4796c176e747bdb537221b7e130a50
fairbairnlauren1-lgtm <fairbairnlauren1@gmail.com>
2026-09-20 16:54:11 -0500

    Test incremental reconciliation of nonmatching bills

--- test_bill_search.py
+++ test_bill_search.py
@@ -27,3 +27,45 @@ def test_find_matches_handles_missing_text():
     bill = {"title": None, "abstract": None}
 
     assert bill_search.find_matches(bill) == {}
+
+
+def test_incremental_reconciliation_removes_updated_nonmatching_bill():
+    old_bills = {
+        "A": {
+            "identifier": "A",
+            "title": "Old justice bill",
+            "abstract": "bail",
+            "latest_action": "Old",
+            "latest_action_date": "2026-01-01",
+        },
+        "B": {
+            "identifier": "B",
+            "title": "Unchanged bill",
+            "abstract": "sentencing",
+            "latest_action": "Old",
+            "latest_action_date": "2026-01-01",
+        },
+    }
+
+    all_bills = [
+        {"identifier": "A", "title": "Old justice bill", "abstract": "highway funding"},
+        {"identifier": "B", "title": "Unchanged bill", "abstract": "sentencing"},
+    ]
+    current_bills = [
+        {
+            "identifier": "B",
+            "title": "Unchanged bill",
+            "abstract": "sentencing",
+            "latest_action": "Old",
+            "latest_action_date": "2026-01-01",
+        }
+    ]
+
+    database_records = dict(old_bills)
+    fetched_ids = {bill.get("identifier", "") for bill in all_bills if bill.get("identifier")}
+    for identifier in fetched_ids:
+        database_records.pop(identifier, None)
+    database_records.update({r["identifier"]: r for r in current_bills if r.get("identifier")})
+
+    assert "A" not in database_records
+    assert "B" in database_records
